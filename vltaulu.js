64d1723c66b2afa2386c8fe2623565a1661fb8ac
Khaled <khaledkelany@gmail.com>
2022-03-24 14:30:32 +0200

    push

---
+++ vltaulu.js
@@ -0,0 +1,8 @@
+// var vlBtn = document.querySelector('.vlbtn')
+// var vlCon = document.querySelector('.vlcontainer')
+// var con = document.querySelector('.container')
+
+// vlBtn.onclick = () => {
+// vlCon.classList.add('vlactive');
+// con.classList.remove('vlactive');
+// }
